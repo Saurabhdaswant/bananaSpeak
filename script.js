709f260a666bae7528eb36def2da8c7897e85fbe
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-05-22 17:53:00 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+//use neog.camp guide for lesson four
+
+//things to cover in this class
+//events , taking inputs from user,network call , error handling ,
+//watch he is doing right now  and after the vedio close it and code it by yourself using guide
