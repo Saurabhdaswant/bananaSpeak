aa0aad25ed7ed1a6c862006dd766fc1af3a9367a
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-05-23 16:12:31 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-//use neog.camp guide for lesson four
+//reference Button using querySelector()
+const button = document.querySelector(".cta");
+//reference textarea
+const inputText = document.querySelector("textarea");
+//reference output div
+const output = document.querySelector(".output");
+//concept of callbacks in javascript
 
-//things to cover in this class
-//events , taking inputs from user,network call , error handling ,
-//watch he is doing right now  and after the vedio close it and code it by yourself using guide
+//the mock url
+const valyrian = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(input) {
+  return valyrian + "?" + "text=" + input;
+}
+
+function errorHandler(error) {
+  console.log("error occured ", error);
+  alert("server is down bruhh");
+}
+
+function callback() {
+  console.log("callback");
+
+  //read the value of the textarea / input
+  const mainText = inputText.value;
+
+  //calling server for processing
+  fetch(getTranslationURL(mainText))
+    .then((response) => response.json())
+    .then((json) => {
+      const translatedText = json.contents.translated;
+      //output
+      output.textContent = translatedText;
+    })
+    .catch(errorHandler);
+}
+
+//add event listener to button
+button.addEventListener("click", callback);
